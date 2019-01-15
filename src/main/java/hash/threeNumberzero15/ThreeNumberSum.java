fa64769c98f85ddd880dda83b513e9ec160da4c2
CalabashBoy <spyuww@gmail.com>
2019-01-15 12:00:09 +0800

    Update ThreeNumberSum.java

--- src/main/java/hash/threeNumberzero15/ThreeNumberSum.java
+++ src/main/java/hash/threeNumberzero15/ThreeNumberSum.java
@@ -9,54 +9,61 @@ import lombok.Data;
 @Data
 class ThreeNumberSum {
 
-  public List<List<Integer>> threeSum(int[] nums) {
-    int len = nums.length;
-    List<List<Integer>> res = new ArrayList<>();
-    Arrays.sort(nums);
-    for (int i = 0; i < len; i++) {
-      int opposite = -nums[i];
-      if (i == 0 || nums[i] != nums[i - 1]) {
-        int left = i + 1, right = len - 1;
-        while (left < right) {
-          int twoSum = nums[left] + nums[right];
-          if (twoSum == opposite) {
-            List<Integer> ans = new ArrayList<>();
-            ans.add(nums[i]);
-            ans.add(nums[left]);
-            ans.add(nums[right]);
-            res.add(ans);
-            left = moveLeft(nums, left, right);
-            right = moveRight(nums, left, right);
-          } else if (twoSum < opposite) {
-            left = moveLeft(nums, left, right);
-          } else {
-            right = moveRight(nums, left, right);
-          }
+ 
+      public List<List<Integer>> threeSum(int[] num)
+    {
+        Arrays.sort(num);
+        List<List<Integer>> res = new LinkedList<>();
+        for (int i = 0; i < num.length - 2; i++)
+        {
+            if (i == 0 || (i > 0 && num[i] != num[i - 1]))
+            {
+                int lo = i + 1, hi = num.length - 1, sum = 0 - num[i];
+                while (lo < hi)
+                {
+                    if (num[lo] + num[hi] == sum)
+                    {
+                        res.add(Arrays.asList(num[i], num[lo], num[hi]));
+                        while (lo < hi && num[lo] == num[lo + 1])
+                            lo++;
+                        while (lo < hi && num[hi] == num[hi - 1])
+                            hi--;
+                        lo++;
+                        hi--;
+                    }
+                    else if (num[lo] + num[hi] < sum)
+                        lo++;
+                    else
+                        hi--;
+                }
+            }
         }
-      }
+        return res;
     }
-    return res;
-  }
 
-  private int moveLeft(int[] nums, int left, int right) {
-    int num = nums[left++];
-    while (left <= right) {
-      if (nums[left] != num) {
-        break;
-      }
-      left++;
-    }
-    return left;
-  }
+    public List<List<Integer>> threeSumSet(int[] nums)
+    {
+        Set<List<Integer>> res = new HashSet<>();
+        if (nums.length == 0)
+            return new ArrayList<>(res);
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++)
+        {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0)
+                    res.add(Arrays.asList(nums[i], nums[j++], nums[k--]));
+                else if (sum > 0)
+                    k--;
+                else if (sum < 0)
+                    j++;
+            }
 
-  private int moveRight(int[] nums, int left, int right) {
-    int num = nums[right--];
-    while (left <= right) {
-      if (nums[right] != num) {
-        break;
-      }
-      right--;
+        }
+        return new ArrayList<>(res);
+
     }
-    return right;
-  }
 }
